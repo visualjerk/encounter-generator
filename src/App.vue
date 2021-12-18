74f6b56c6274c14a8b7d33c0c43da8c0b823823f
visualjerk <jb@visualjerk.de>
2021-12-18 18:17:12 +0100

    text styling

--- src/App.vue
+++ src/App.vue
@@ -23,56 +23,66 @@ export default defineComponent({
       class="absolute top-0 left-0 w-full h-full object-cover opacity-10"
     />
     <div>
-      <h1 class="mb-5 text-indigo-300">Encounter Generator</h1>
+      <h1 class="mb-10 text-indigo-400">
+        <span class="font-extrabold text-indigo-300 uppercase">
+          Encounter
+        </span>
+        <span class="tracking-wide">Generator</span>
+      </h1>
       <form>
-        <fieldset class="flex gap-1">
-          <legend class="mb-2">Choose your environment:</legend>
-          <div
-            v-for="(id, name) in environments"
-            :key="id"
-            class="group relative w-40 h-40 bg-right-top bg-cover"
-            :style="{
-              backgroundImage: `url('${environmentImages[id]}')`,
-            }"
-          >
-            <label
-              :for="`environment-${id}`"
-              class="
-                text-indigo-100 text-lg
-                font-medium
-                tracking-wider
-                h-full
-                w-full
-                flex
-                items-center
-                justify-center
-                relative
-                z-10
-                cursor-pointer
-              "
-              >{{ name }}</label
-            >
-            <input
-              type="radio"
-              :value="id"
-              :id="`environment-${id}`"
-              name="environment"
-              class="peer appearance-none"
-              v-model="selectedEnvironment"
-            />
+        <fieldset>
+          <legend class="mb-2 text-lg tracking-wide text-center capitalize">
+            Choose your environment
+          </legend>
+          <div class="flex gap-1 bg-indigo-200 p-1">
             <div
-              class="
-                absolute
-                top-0
-                left-0
-                h-full
-                w-full
-                bg-indigo-800
-                opacity-70
-                group-hover:opacity-60
-                peer-checked:opacity-40
-              "
-            />
+              v-for="(id, name) in environments"
+              :key="id"
+              class="group relative w-40 h-40 bg-right-top bg-cover"
+              :style="{
+                backgroundImage: `url('${environmentImages[id]}')`,
+              }"
+            >
+              <label
+                :for="`environment-${id}`"
+                class="
+                  text-indigo-100 text-lg
+                  font-medium
+                  tracking-wider
+                  h-full
+                  w-full
+                  flex
+                  items-center
+                  justify-center
+                  relative
+                  z-10
+                  cursor-pointer
+                  drop-shadow-lg
+                "
+                >{{ name }}</label
+              >
+              <input
+                type="radio"
+                :value="id"
+                :id="`environment-${id}`"
+                name="environment"
+                class="peer appearance-none"
+                v-model="selectedEnvironment"
+              />
+              <div
+                class="
+                  absolute
+                  top-0
+                  left-0
+                  h-full
+                  w-full
+                  bg-indigo-800
+                  opacity-70
+                  group-hover:opacity-60
+                  peer-checked:opacity-40
+                "
+              />
+            </div>
           </div>
         </fieldset>
       </form>
